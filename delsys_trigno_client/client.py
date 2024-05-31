1121f847a48de9d50f1e0c3adacec7c35da682de
Louis Gevers <louis.gevers@gmail.com>
2024-05-31 15:38:36 +0100

    Add command interface and sensor pairing

--- delsys_trigno_client/client.py
+++ delsys_trigno_client/client.py
@@ -1,15 +1,49 @@
 import socket
+import select
 from typing import Optional
 
 from delsys_trigno_client.constants import DEFAULT_DIGITAL_SERVER_IP, TrignoPort
 
-class TrignoClient():
-    def __init__(self, digital_server_ip: str = DEFAULT_DIGITAL_SERVER_IP, timeout: Optional[float] = 5) -> None:
+
+class TrignoClient:
+    def __init__(
+        self,
+        digital_server_ip: str = DEFAULT_DIGITAL_SERVER_IP,
+        timeout: Optional[float] = 5,
+    ) -> None:
         self._sockets: dict[TrignoPort, socket.socket] = dict()
         for port in TrignoPort:
-            self._sockets[port] = socket.create_connection((digital_server_ip, port.value), timeout=timeout)
+            self._sockets[port] = socket.create_connection(
+                (digital_server_ip, port.value), timeout=timeout
+            )
+        # First response is protocol version
+        self._protocol_version = self._receive_response(TrignoPort.COMMAND)
+        # Make connection master
+        self._send_command("MASTER")
+
+    def is_sensor_paired(self, id: int) -> bool:
+        resp = self._send_command(f"SENSOR {id} PAIRED?")
+        return resp == "YES"
+
+    def pair_sensor(self, id: int) -> bool:
+        resp = self._send_command(f"SENSOR {id} PAIR")
+        # Check if initiated
+        if "INITIATED" not in resp:
+            return False
+        # Wait for pairing to complete
+        resp = self._receive_response(TrignoPort.COMMAND)
+        return "COMPLETE" in resp
 
     def close(self):
         for socket in self._sockets.values():
             socket.close()
-            
+
+    def _send_command(self, command: str):
+        message = f"{command}\r\n\r\n"
+        self._sockets[TrignoPort.COMMAND].sendall(message.encode("ascii"))
+        return self._receive_response(TrignoPort.COMMAND)
+
+    def _receive_response(self, port: TrignoPort) -> str:
+        # Wait for read to be available
+        select.select([self._sockets[port]], [], [])
+        return self._sockets[port].recv(1024).decode("ascii").strip()

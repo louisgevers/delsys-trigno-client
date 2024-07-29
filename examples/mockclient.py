15e3cc523667811683f9f5110327104d37dfe7dd
Louis Gevers <louis.gevers@gmail.com>
2024-07-29 14:12:59 +0100

    Update mockserver

--- examples/mockclient.py
+++ examples/mockclient.py
@@ -1,15 +1,17 @@
 import time
 import pandas as pd
 
+from argparse import ArgumentParser, Namespace
+
 from delsys_trigno_client import TrignoClient
 
 
-def run_client():
-    client = TrignoClient(digital_server_ip="localhost")  # Mocked server
+def run_client(debug: bool, duration: float):
+    client = TrignoClient(digital_server_ip="localhost") if debug else TrignoClient()
     client.pair_sensor(id=1)
 
     client.start_acquisition()
-    time.sleep(3)
+    time.sleep(duration)
     client.stop_acquisition()
 
     readings = client.get_readings_emg()
@@ -18,6 +20,12 @@ def run_client():
 
     client.close()
 
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--debug", action="store_true")
+    parser.add_argument("-t", "--time", type=int, default=5)
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    run_client()
+    args = parse_args()
+    run_client(args.debug, args.time)
